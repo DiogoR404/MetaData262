9e36df679e011e62fcae40842b4472e9527697b7
migueltrigo <36707538+migueltrigo@users.noreply.github.com>
2022-04-21 14:51:42 +0100

    Update mixedAnalysis.js

--- mixedAnalysis.js
+++ mixedAnalysis.js
@@ -32,149 +32,169 @@ var table_syntax = table[2]
 var table_operators = table[3]
 var table_global=table[4]
 
-var results = {}
-for (v in results_dynamic){
-    if (v ==="notSupported"){
+
+function mixedAnalysisNode(){
+    var results = {}
+    for (v in results_dynamic){
+        if (v ==="notSupported"){
+            for (test in results_dynamic[v]){
+                var fileToAnalyse = results_dynamic[v][test]["path"];
+                if (results.hasOwnProperty(v)){           
+                    results[v].push(fileToAnalyse);
+                }else{
+                    results[v] = [fileToAnalyse];
+                }  
+            }      
+            continue
+        }
         for (test in results_dynamic[v]){
+            var version = v;
+
+            //loads the test
             var fileToAnalyse = results_dynamic[v][test]["path"];
-            if (results.hasOwnProperty(v)){           
-                results[v].push(fileToAnalyse);
+            var program_text = readFileContent(fileToAnalyse);
+
+            //checks if test belongs to intl
+            if (fileToAnalyse.includes("intl")){
+                version = "intl"
+            }
+            //checks if test belongs to property-escapes
+            else if (fileToAnalyse.includes("property-escapes")){
+                version = "property-escapes"
+            }
+
+            else{
+                //if test is module-code or block-scope belongs to es6 version
+                if (fileToAnalyse.includes("module-code") || fileToAnalyse.includes("arrow") || fileToAnalyse.includes("generator") || fileToAnalyse.includes("dstr")){
+                    if(isLowerVersion(version, "es6")){
+                        version = "es6"
+                    }
+                }
+                if (fileToAnalyse.includes("async")){
+                    if(isLowerVersion(version, "es8")){
+
+                        version = "es8"
+                    }
+                }
+                //if test belongs to named-groups has version es9
+                if (fileToAnalyse.includes("named-groups") ){
+                    if(isLowerVersion(version, "es9")){
+
+                        version ="es9"
+                    }
+                }
+
+                //use of esprima to analyse the test
+                try{
+                    var program = p(program_text);
+                    var version = ta.analysis(program, table_func, table_vars, table_syntax, table_operators, table_global, version);
+
+                } catch(e){
+                    //fault in esprima
+                    //var version = "notSupported";
+                }  
+            }
+
+            //adds the test to the correspondent version
+            if (results.hasOwnProperty(version)){           
+                results[version].push(fileToAnalyse);
             }else{
-                results[v] = [fileToAnalyse];
-            }  
-        }      
-        continue
+                results[version] = [fileToAnalyse];
+            }
+
+        }
     }
-    for (test in results_dynamic[v]){
-        var version = v;
-
-        //loads the test
-        var fileToAnalyse = results_dynamic[v][test]["path"];
-        var program_text = readFileContent(fileToAnalyse);
-
-        //checks if test belongs to intl
-        if (fileToAnalyse.includes("intl")){
-            version = "intl"
-        }
-        //checks if test belongs to property-escapes
-        else if (fileToAnalyse.includes("property-escapes")){
-            version = "property-escapes"
-        }
-        
-        else{
-            //if test is module-code or block-scope belongs to es6 version
-            if (fileToAnalyse.includes("module-code") || fileToAnalyse.includes("arrow") || fileToAnalyse.includes("generator") || fileToAnalyse.includes("dstr")){
-                if(isLowerVersion(version, "es6")){
-                    version = "es6"
-                }
-            }
-            if (fileToAnalyse.includes("async")){
-                if(isLowerVersion(version, "es8")){
-
-                    version = "es8"
-                }
-            }
-            //if test belongs to named-groups has version es9
-            if (fileToAnalyse.includes("named-groups") ){
-                if(isLowerVersion(version, "es9")){
-
-                    version ="es9"
-                }
-            }
-
-            //use of esprima to analyse the test
-            try{
-                var program = p(program_text);
-                var version = ta.analysis(program, table_func, table_vars, table_syntax, table_operators, table_global, version);
-                
-            } catch(e){
-                //fault in esprima
-                //var version = "notSupported";
-            }  
-        }
-
-        //adds the test to the correspondent version
-        if (results.hasOwnProperty(version)){           
-            results[version].push(fileToAnalyse);
-        }else{
-            results[version] = [fileToAnalyse];
-        }
-
-    }
+    let data2 = JSON.stringify(results);
+    fs.writeFileSync('./dynamic_results/mixed_analysis.json', data2);
 }
-let data2 = JSON.stringify(results);
-fs.writeFileSync('./dynamic_results/mixed_analysis.json', data2);
-var results = {}
 
-for (v in results_spidermonkey){
-    if (v ==="notSupported"){
+function mixedAnalysisSM(){
+    var results = {}
+
+    for (v in results_spidermonkey){
+        if (v ==="notSupported"){
+            for (test in results_spidermonkey[v]){
+                var fileToAnalyse = results_spidermonkey[v][test]["path"];
+                if (results.hasOwnProperty(v)){           
+                    results[v].push(fileToAnalyse);
+                }else{
+                    results[v] = [fileToAnalyse];
+                }   
+            }     
+            continue
+        }
         for (test in results_spidermonkey[v]){
+            var version = v;
+
+            //loads the test
             var fileToAnalyse = results_spidermonkey[v][test]["path"];
-            if (results.hasOwnProperty(v)){           
-                results[v].push(fileToAnalyse);
+            var program_text = readFileContent(fileToAnalyse);
+
+            //checks if test belongs to intl
+            if (fileToAnalyse.includes("intl")){
+                version = "intl"
+            }
+            //checks if test belongs to property-escapes
+            else if (fileToAnalyse.includes("property-escapes")){
+                version = "property-escapes"
+            }
+
+            else{
+                //if test is module-code or block-scope belongs to es6 version
+                if (fileToAnalyse.includes("module-code") || fileToAnalyse.includes("arrow") || fileToAnalyse.includes("generator") || fileToAnalyse.includes("dstr")){
+                    if(isLowerVersion(version, "es6")){
+                        version = "es6"
+                    }
+                }
+                if (fileToAnalyse.includes("async")){
+                    if(isLowerVersion(version, "es8")){
+
+                        version = "es8"
+                    }
+                }
+                //if test belongs to named-groups has version es9
+                if (fileToAnalyse.includes("named-groups") ){
+                    if(isLowerVersion(version, "es9")){
+
+                        version ="es9"
+                    }
+                }
+
+                //use of esprima to analyse the test
+                try{
+                    var program = p(program_text);
+                    var version = ta.analysis(program, table_func, table_vars, table_syntax, table_operators, table_global, version);
+
+                } catch(e){
+                    //fault in esprima
+                    //var version = "notSupported";
+                }  
+            }
+
+            //adds the test to the correspondent version
+            if (results.hasOwnProperty(version)){           
+                results[version].push(fileToAnalyse);
             }else{
-                results[v] = [fileToAnalyse];
-            }   
-        }     
-        continue
+                results[version] = [fileToAnalyse];
+            }
+        }
     }
-    for (test in results_spidermonkey[v]){
-        var version = v;
-
-        //loads the test
-        var fileToAnalyse = results_spidermonkey[v][test]["path"];
-        var program_text = readFileContent(fileToAnalyse);
-
-        //checks if test belongs to intl
-        if (fileToAnalyse.includes("intl")){
-            version = "intl"
-        }
-        //checks if test belongs to property-escapes
-        else if (fileToAnalyse.includes("property-escapes")){
-            version = "property-escapes"
-        }
-        
-        else{
-            //if test is module-code or block-scope belongs to es6 version
-            if (fileToAnalyse.includes("module-code") || fileToAnalyse.includes("arrow") || fileToAnalyse.includes("generator") || fileToAnalyse.includes("dstr")){
-                if(isLowerVersion(version, "es6")){
-                    version = "es6"
-                }
-            }
-            if (fileToAnalyse.includes("async")){
-                if(isLowerVersion(version, "es8")){
-
-                    version = "es8"
-                }
-            }
-            //if test belongs to named-groups has version es9
-            if (fileToAnalyse.includes("named-groups") ){
-                if(isLowerVersion(version, "es9")){
-
-                    version ="es9"
-                }
-            }
-
-            //use of esprima to analyse the test
-            try{
-                var program = p(program_text);
-                var version = ta.analysis(program, table_func, table_vars, table_syntax, table_operators, table_global, version);
-                
-            } catch(e){
-                //fault in esprima
-                //var version = "notSupported";
-            }  
-        }
-
-        //adds the test to the correspondent version
-        if (results.hasOwnProperty(version)){           
-            results[version].push(fileToAnalyse);
-        }else{
-            results[version] = [fileToAnalyse];
-        }
-
-    }
+    let data3 = JSON.stringify(results);
+    fs.writeFileSync('./dynamic_results/mixed_analysis_sm.json', data3);
+    
 }
 
-let data3 = JSON.stringify(results);
-fs.writeFileSync('./dynamic_results/mixed_analysis_sm.json', data3);
+
+if (process.argv.length() === 2){
+    mixedAnalysisNode()
+    mixedAnalysisSM()
+}
+else if (process.argv[2] === "node"){
+    mixedAnalysisNode()
+}
+else if (process.argv[2] === "spidermonkey" ){
+    mixedAnalysisSM()
+}
+else{
+    console.log("Wrong Argument")
+}
