f0c02f8d472c893ebbccccee323be1fe7a8bda82
DiogoR404 <ist187526@tecnico.ulisboa.pt>
2023-02-11 22:11:09 +0000

    fix duplicate function

--- resources/harness/harness_f_module.js
+++ resources/harness/harness_f_module.js
@@ -637,25 +637,6 @@ function allowProxyTraps(overrides) {
   };
 }
 
-//hidden-constructors.js
-// Copyright (C) 2020 Rick Waldron. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-/*---
-description: |
-  Provides uniform access to built-in constructors that are not exposed to the global object.
-defines:
-  - AsyncArrowFunction
-  - AsyncFunction
-  - AsyncGeneratorFunction
-  - GeneratorFunction
----*/
-
-var AsyncArrowFunction = Object.getPrototypeOf(async () => {}).constructor;
-var AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
-var AsyncGeneratorFunction = Object.getPrototypeOf(async function* () {}).constructor;
-var GeneratorFunction = Object.getPrototypeOf(function* () {}).constructor;
-
 //decimalToHexString.js
 // Copyright (C) 2017 André Bargull. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
